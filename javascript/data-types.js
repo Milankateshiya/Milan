ac09a89aa1d841874fcfee7a1758b7748bbd216e
milan <milankateshiya/10aug_milan>
2024-04-26 19:02:21 +0530

    upload

---
+++ javascript/data-types.js
@@ -0,0 +1,23 @@
+// string
+'milan kateshiya'
+
+// number
+9979149339
+
+
+// boolean
+true /false
+
+// null
+let a =null;
+// undefined
+let b = undefined;
+
+// var z= 10
+let z=20;
+
+a=10;
+a='10;'
+a=b;
+a={};
+
